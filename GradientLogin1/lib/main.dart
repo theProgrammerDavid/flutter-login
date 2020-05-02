a6d7e7244cc7681f954ecf032adfed993aee7bbf
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-02 20:26:36 +0530

    [WIP]: text fields

--- GradientLogin1/lib/main.dart
+++ GradientLogin1/lib/main.dart
@@ -16,9 +16,9 @@ class HomePage extends StatelessWidget {
           width: double.infinity,
           decoration: BoxDecoration(
               gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-            Colors.green[900],
-            Colors.green[800],
-            Colors.green[600]
+            Colors.orange[900],
+            Colors.orange[800],
+            Colors.orange[600]
           ])),
           child: Column(children: <Widget>[
             SizedBox(height: 80),
@@ -29,14 +29,38 @@ class HomePage extends StatelessWidget {
                     children: <Widget>[
                       Text("Login",
                           style: TextStyle(color: Colors.white, fontSize: 50)),
-                      SizedBox(height: 30),
+                      SizedBox(height: 10),
                       Text("Welcome Back",
                           style: TextStyle(color: Colors.white, fontSize: 15)),
                     ])),
             SizedBox(height: 20),
             Expanded(
                 child: Container(
-              decoration: BoxDecoration(color: Colors.white, borderRadius:BorderRadius.only(topLeft: Radius.circular(borderRadiusValue), topRight: Radius.circular(borderRadiusValue))),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(borderRadiusValue),
+                      topRight: Radius.circular(borderRadiusValue))),
+              child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(children: <Widget>[
+                    SizedBox(height:20),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color.fromRGBO(225, 95, 27, .3),
+                                blurRadius: 20,
+                                offset: Offset(0, 10))
+                          ],
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white),
+                          child: Column(children: <Widget>[
+                            Container()
+                          ]),
+                    ),
+                  ])),
             )),
           ]),
         ),
