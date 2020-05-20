ac34a2cc0dc12239081aea2da8d97918d1d26e21
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-20 16:43:22 +0530

    fix: no overflow

--- GradientLogin1/lib/main.dart
+++ GradientLogin1/lib/main.dart
@@ -37,143 +37,154 @@ class HomePage extends StatelessWidget {
                     ])),
             SizedBox(height: 20),
             Expanded(
-                child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(borderRadiusValue),
-                      topRight: Radius.circular(borderRadiusValue))),
-              child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(children: <Widget>[
-                    SizedBox(height: 20),
-                    Container(
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                                color: Color.fromRGBO(225, 95, 27, .3),
-                                blurRadius: 20,
-                                offset: Offset(0, 10))
-                          ],
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white),
-                      child: Column(children: <Widget>[
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(borderRadiusValue),
+                          topRight: Radius.circular(borderRadiusValue))),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Wrap(
+                      direction: Axis.horizontal,
+                      children: [
+                        SizedBox(height: 20),
                         Container(
-                          padding: EdgeInsets.all(10),
+                          padding: EdgeInsets.all(20),
                           decoration: BoxDecoration(
-                              border: Border(
-                                  bottom: BorderSide(color: Colors.grey[200]))),
-                          child: TextField(
-                              autofocus: true,
-                              decoration: InputDecoration(
-                                  hintText: "Email or username",
-                                  border: InputBorder.none,
-                                  hintStyle:
-                                      TextStyle(color: Colors.grey[500]))),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Color.fromRGBO(225, 95, 27, .3),
+                                    blurRadius: 20,
+                                    offset: Offset(0, 10))
+                              ],
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white),
+                          child: Column(children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom:
+                                          BorderSide(color: Colors.grey[200]))),
+                              child: TextField(
+                                  autofocus: true,
+                                  decoration: InputDecoration(
+                                      hintText: "Email or username",
+                                      border: InputBorder.none,
+                                      hintStyle:
+                                          TextStyle(color: Colors.grey[500]))),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom:
+                                          BorderSide(color: Colors.grey[200]))),
+                              child: TextField(
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                      hintText: "Password",
+                                      border: InputBorder.none,
+                                      hintStyle:
+                                          TextStyle(color: Colors.grey[500]))),
+                            ),
+                          ]),
                         ),
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              border: Border(
-                                  bottom: BorderSide(color: Colors.grey[200]))),
-                          child: TextField(
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                  hintText: "Password",
-                                  border: InputBorder.none,
-                                  hintStyle:
-                                      TextStyle(color: Colors.grey[500]))),
-                        ),
-                      ]),
-                    ),
-                    // SizedBox(height: 20),
-                    // FlatButton(
-                    //     onPressed: () => {},
-                    //     child: Text(
-                    //       "Forgot Password?",
-                    //       style: TextStyle(color: Colors.grey),
-                    //     )),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10),
-                      child: FlatButton(
-                          onPressed: () => {},
-                          child: Text(
-                            "Forgot Password?",
-                            style: TextStyle(color: Colors.grey),
-                          )),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10),
-                      child: FlatButton(
-                          onPressed: () => {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => createAccount(),
-                                    ))
-                              },
-                          child: Text(
-                            "Create Account",
-                            style: TextStyle(color: Colors.grey),
-                          )),
-                    ),
-                    //SizedBox(height: 40),
 
-                    Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          gradient: LinearGradient(
-                              // begin: Alignment.topCenter,
-                              colors: [
-                                Colors.orange[900],
-                                Colors.orange[800],
-                                Colors.orange[600]
-                              ])),
-                      child: Container(
-                        width: 250,
-                        child: FlatButton(
-                            onPressed: () => {print("Sign in button pressed")},
-                            child: Text("Sign In",
-                                style: TextStyle(color: Colors.white))),
-                      ),
+                        //SizedBox(height: 40),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 60),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 10),
+                                  child: FlatButton(
+                                      onPressed: () => {},
+                                      child: Text(
+                                        "Forgot Password?",
+                                        style: TextStyle(color: Colors.grey),
+                                      )),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 10),
+                                  child: FlatButton(
+                                      onPressed: () => {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      createAccount(),
+                                                ))
+                                          },
+                                      child: Text(
+                                        "Create Account",
+                                        style: TextStyle(color: Colors.grey),
+                                      )),
+                                ),
+                                Container(
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      gradient: LinearGradient(
+                                          // begin: Alignment.topCenter,
+                                          colors: [
+                                            Colors.orange[900],
+                                            Colors.orange[800],
+                                            Colors.orange[600]
+                                          ])),
+                                  child: Container(
+                                    width: 250,
+                                    child: FlatButton(
+                                        onPressed: () =>
+                                            {print("Sign in button pressed")},
+                                        child: Text("Sign In",
+                                            style: TextStyle(
+                                                color: Colors.white))),
+                                  ),
+                                ),
+                                SizedBox(height: 20),
+                                Text("Continue with Social Media",
+                                    style: TextStyle(
+                                        color: Colors.grey, fontSize: 10)),
+                                SizedBox(height: 20),
+                              ]),
+                        ),
+                        SizedBox(height: 50),
+                        Row(children: <Widget>[
+                          Expanded(
+                            child: Container(
+                                height: 50,
+                                child: FlatButton(
+                                  onPressed: () => {},
+                                  child: Text("Sign in with Facebook",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 12)),
+                                ),
+                                decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.circular(40))),
+                          ),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Container(
+                                child: FlatButton(
+                                  onPressed: () => {},
+                                  child: Text("Sign in with Google",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 12)),
+                                ),
+                                height: 50,
+                                decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(40))),
+                          ),
+                        ]),
+                      ],
                     ),
-                    SizedBox(height: 50),
-                    Text("Continue with Social Media",
-                        style: TextStyle(color: Colors.grey, fontSize: 10)),
-                    SizedBox(height: 50),
-                    Row(children: <Widget>[
-                      Expanded(
-                        child: Container(
-                            height: 50,
-                            child: FlatButton(
-                              onPressed: () => {},
-                              child: Text("Sign in with Facebook",
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 12)),
-                            ),
-                            decoration: BoxDecoration(
-                                color: Colors.blue,
-                                borderRadius: BorderRadius.circular(40))),
-                      ),
-                      SizedBox(width: 20),
-                      Expanded(
-                        child: Container(
-                            child: FlatButton(
-                              onPressed: () => {},
-                              child: Text("Sign in with Google",
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 12)),
-                            ),
-                            height: 50,
-                            decoration: BoxDecoration(
-                                color: Colors.black,
-                                borderRadius: BorderRadius.circular(40))),
-                      ),
-                    ]),
-                  ])),
-            )),
+                  )),
+            )
           ]),
         ),
       ),
