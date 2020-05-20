33fc487ea7837fe56e794e43ef16779e7c335574
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-20 16:22:07 +0530

    [WIP] Create Account page

---
+++ GradientLogin1/lib/createAccount.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class createAccount extends StatefulWidget {
+  createAccount({Key key}) : super(key: key);
+
+  @override
+  _createAccountState createState() => _createAccountState();
+}
+
+class _createAccountState extends State<createAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Scaffold(body:Text("Sdsssssssssssssssssssssssssss")),
+    );
+  }
+}
